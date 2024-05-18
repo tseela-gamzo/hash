279a9ff36a66df5a9a4e45d8dd3aa63160c210f7
tseela-gamzo <151513296+tseela-gamzo@users.noreply.github.com>
2024-05-18 17:32:57 +0300

    edited club server to look more appealing

--- club/app.py
+++ club/app.py
@@ -1,15 +1,19 @@
 # An object of Flask class is our WSGI application.
 from flask import Flask, render_template, request, redirect
 import os
+import logging
  
 # Flask constructor takes the name of current module (__name__) as argument.
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
     username = request.form['username']
     password = request.form['password']
+
+    logging.debug(f'Received user with name: {username}')
 
     #
     #
@@ -35,4 +39,4 @@ def index():
 # main driver function
 if __name__ == '__main__':
     # run() method of Flask class runs the application on the local development server.
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
